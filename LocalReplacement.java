0bfde5b611a2797c500854852178a488a74008bb
Pray Chow <1007950881@qq.com>
2019-11-15 23:54:29 +0800

    Local Replacement

--- LocalReplacement.java
+++ LocalReplacement.java
@@ -13,7 +13,11 @@ public class LocalReplacement {
         adjacentMatrix = new int[n+1][n+1];
     }
 
-
+    /**
+     * 读取随机图文件，将图信息保存到map中进行局部替换处理
+     * @param file 路径文件
+     * @return 返回随机图标识和邻接矩阵的map
+     */
     static HashMap<String, int[][]> readFromFile(File file){
         HashMap<String, int[][]> map = new HashMap<>();
         ArrayList<String> arrayList = new ArrayList<>();
@@ -29,10 +33,16 @@ public class LocalReplacement {
         } catch(IOException e){
             e.printStackTrace();
         }
+//        System.out.println("arrayList.size() " + arrayList.size());
         for(int i = 0; i < arrayList.size(); ){
+            // 随机图标识
             if(arrayList.get(i).contains("+")){
                 String nk = arrayList.get(i);
-                int n = Integer.parseInt(nk.charAt(0)+"");
+//                System.out.println("nk " + nk);
+                String strnk[] = nk.split("\\+");
+                // 随机图顶点个数
+                int n = Integer.parseInt(strnk[0]);
+                // 将随机图矩阵保存
                 int [][]adjMtr = new int[n+1][n+1];
                 for(int j = i+1; j <= i+n; j++){
                     String []temp = arrayList.get(j).split(" ");
@@ -40,6 +50,7 @@ public class LocalReplacement {
                         adjMtr[j-i][k+1] = Integer.parseInt(temp[k]);
                     }
                 }
+                // map保存随机图标识和邻接矩阵
                 map.put(nk, adjMtr);
                 i = i + n + 1;
             }
@@ -50,34 +61,13 @@ public class LocalReplacement {
     }
 
     public static void main(String []args){
-        File file = new File("D:\\projects\\vertexCover\\src\\Rand3RegGraph.txt");
+        File file = new File("C:\\Users\\zhoup\\Documents\\project\\java\\src\\Rand3RegGraph.txt");
         if(!file.exists()){
             System.out.println("file does not exist!");
         }
 
         HashMap<String, int[][]> map = readFromFile(file);
-        System.out.println(map.size());
-        int n = 6;
-        int adj[][] = map.get("6+0");
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                System.out.print(adj[i][j] + " ");
-            }System.out.println();
-        }
-        n = 50;
-        adj = map.get("50+1");
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                System.out.print(adj[i][j] + " ");
-            }System.out.println();
-        }
-        n = 1000;
-        adj = map.get("1000+9");
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= n; j++){
-                System.out.print(adj[i][j] + " ");
-            }System.out.println();
-        }
+
     }
 
 }
